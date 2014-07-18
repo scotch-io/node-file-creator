6b93b6c81df6247f718a2f969caf8f31c273467a
Chris Sevilleja <chris@scotch.io>
2014-07-18 09:00:22 -0700

    testing json downloads

--- server.js
+++ server.js
@@ -5,8 +5,25 @@ app.get('/', function(req, res) {
 	res.send('whats up');
 });
 
+var sample = {
+  "name": "node-file-creator",
+  "main": "server.js",
+  "dependencies": {
+    "express": "^4.6.1"
+  }
+}
+
+var text = {
+	"hello.json": sample,
+	"bye.json": "Goodbye Cruel World!" 
+};
+
 app.get('/download', function(req, res) {
-	
+	res.set({ 
+		"Content-Disposition":"attachment; filename=\"Hello.json\"",
+		"Content-Type": "application/json"
+	});
+	res.send(text['hello.json']);
 });
 
 app.listen(8080);
